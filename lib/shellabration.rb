c93cb0a627b1c55cc43c977d3b3fc15bc08aa392
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-01-17 13:40:14 -0500

    First commit and configuration

---
+++ lib/shellabration.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'shellabration/version'
+
+module Shellabration
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/shellabration/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Shellabration
+  VERSION = '0.0.1'
+end
